ef596f703e4e25c10c7753cc3e9c17a5f1d583d3
Alexandr <nomto_04@mail.ru>
2018-11-29 20:45:51 +0800

    Это наш первый коммит

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+// var incr = 10,
+// 	decr = 10;
+// document.write(incr++);
+// document.write("<br />");
+// document.write(decr--);
+
+// document.write("100" === 100);
+
+var isCheked	= false,
+	isClose		= false;
+document.write(!isCheked || !isClose);
